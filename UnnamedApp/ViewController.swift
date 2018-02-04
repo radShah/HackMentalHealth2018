9c0ab67d2d4645dbf78a149ac8b727408853065f
Radha <radha.shah@gmail.com>
2018-02-03 18:07:38 -0800

    3 buttons

--- UnnamedApp/ViewController.swift
+++ UnnamedApp/ViewController.swift
@@ -9,27 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
         
+        let answerButton1 = UIButton()
+        addRestraintsToButton(answerButton: answerButton1, verticalConstraint: 100)
+
+        let answerButton2 = UIButton()
+        addRestraintsToButton(answerButton: answerButton2, verticalConstraint: 175)
+        
+        let answerButton3 = UIButton()
+        addRestraintsToButton(answerButton: answerButton3, verticalConstraint: 250)
+
+    }
+    
+    func addRestraintsToButton(answerButton: UIButton, verticalConstraint: Int) {
+        answerButton.backgroundColor = UIColor.green
+        answerButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(answerButton)
+        
+        let leadingMarginContraints = NSLayoutConstraint(item: answerButton, attribute:.leadingMargin, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 0)
+        let trailingMarginContraints = NSLayoutConstraint(item: answerButton, attribute:.trailingMargin, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: 40)
+        let verticalConstraint = NSLayoutConstraint(item: answerButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: CGFloat(verticalConstraint))
+        let heightConstraint = NSLayoutConstraint(item: answerButton, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
+        view.addConstraints([leadingMarginContraints, trailingMarginContraints, verticalConstraint, heightConstraint])
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    fileprivate func createFirstAnswerButton() {
         let answerButton1 = UIButton()
         answerButton1.backgroundColor = UIColor.green
         answerButton1.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(answerButton1)
         
-        answerButton1.translatesAutoresizingMaskIntoConstraints = false
-        let horizontalConstraint = NSLayoutConstraint(item: answerButton1, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
-        let verticalConstraint = NSLayoutConstraint(item: answerButton1, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)
-        let widthConstraint = NSLayoutConstraint(item: answerButton1, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
-        let heightConstraint = NSLayoutConstraint(item: answerButton1, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
-        view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
     }
 
 
